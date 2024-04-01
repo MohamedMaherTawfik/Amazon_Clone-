54b9d4cf9e35fa454c63e397f3ed1cf54590f2dd
MOHAMED MAHER <141060515+MohamedMaherTawfik@users.noreply.github.com>
2024-04-01 17:57:05 +0200

    add sign_up api

--- project/settings.py
+++ project/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
-    'dj_rest_auth',
+    'dj_rest_auth', 
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'dj_rest_auth.registration',
     
         
     #My APPS:
@@ -58,10 +62,12 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 INTERNAL_IPS = [
@@ -69,6 +75,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+SITE_ID = 1
 
 ROOT_URLCONF = 'project.urls'
 
@@ -165,3 +173,6 @@ REST_AUTH = {
     'USE_JWT': True,
     'JWT_AUTH_COOKIE': 'jwt-auth',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
